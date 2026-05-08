7807246c3158973a0d250f4ad9ee333649e4c94f
louis <louis.chevallier@gmail.com>
2026-05-08 17:39:57 +0200

    client-code

--- client.py
+++ client.py
@@ -22,6 +22,10 @@ with open("livre.txt", "r") as fd :
 	lines = fd.readlines()
 	print(len(lines))
 
+	S = 100
+	blocks = [ '\n'.join(lines[i:i+S]) for i,l in enumerate(lines[::S))]
+
+	print(len(blocks)
 
 response = httpx.post(f"{BASE_URL}/audio/speech", json=payload, timeout=120.0)
 response.raise_for_status()
