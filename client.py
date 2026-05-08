86f7e093071636d9c2b8802467d12dd0ff1e4f58
louis <louis.chevallier@gmail.com>
2026-05-08 17:21:37 +0200

    client-code

--- client.py
+++ client.py
@@ -1,6 +1,13 @@
 import io
 import httpx
 import soundfile as sf
+# Source - https://stackoverflow.com/a/10359645
+# Posted by huon, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-05-08, License - CC BY-SA 4.0
+
+import numpy as np
+from scipy.io.wavfile import write
+
 
 BASE_URL = "http://localhost:8000/v1"
  
@@ -19,3 +26,10 @@ print(f"Got audio: {len(audio_array)} samples at {sr} Hz")
 
 print(audio_array)
 # you can play the audio with a library like `sounddevice.play` for example
+
+
+rate = 44100
+data = audio_array
+scaled = np.int16(data / np.max(np.abs(data)) * 32767)
+write('test.wav', rate, scaled)
+
