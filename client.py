2064549a237ed5ec3accfa1c2b7807ef0b028862
louis <louis.chevallier@gmail.com>
2026-05-08 20:41:51 +0200

    code+

--- client.py
+++ client.py
@@ -20,8 +20,8 @@ payload = {
     "input": "Paris is a beautiful city!",
     "model": "mistralai/Voxtral-4B-TTS-2603", # "voxtral-mini-tts-2603",
     "response_format": "wav",
-#    "voice": "Marie_excited",
-	"voice_id": "fr_marie_excited"	
+    "voice": "FR_Marie_excited"
+#	"voice_id": "fr_marie_excited"	
 }
 
 with open("livre.txt", "r") as fd :
